13a0e189fdead74fb62a474ac563428051f3011f
Juan Manuel Mena Hernandez <jmmenah@gmail.com>
2023-02-07 20:15:15 +0100

    Rutas Ejemplos

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Loginformik from '../../components/pure/forms/loginFormik';
 
 const Loginpage = () => {
     return (
         <div>
             <h1>Login Page</h1>
+            <Loginformik></Loginformik>
         </div>
     );
 }
 
 export default Loginpage;
+
